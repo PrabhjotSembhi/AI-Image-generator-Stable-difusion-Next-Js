c7393e2c7e496bf57bf0de46e887b987907deb80
PrabhjotSembhi <prabh.s2508@gmail.com>
2023-02-21 14:38:33 +0530

    Replicate API backend Connection

--- src/pages/api/generate.ts
+++ src/pages/api/generate.ts
@@ -4,7 +4,7 @@ type Data = {
     name: string
 }
 
-export default function handler(
+export default async function handler(
     req: NextApiRequest,
     res: NextApiResponse<Data>
 ){
@@ -28,7 +28,38 @@ export default function handler(
             }
         })
 
-    })
+    });
 
-    res.status(200).json({name: 'john'})
+    let startResponse = await firstResponse.json();
+    let endpointURL = startResponse.urls.get;
+
+    let generatedImage = null;
+
+    while (!generatedImage) {
+        let finalResponse = await fetch(endpointURL, {
+            method: "GET",
+            headers:{
+                "Content-Type" : "application/json",
+                Authorization: "Token" + "b4dd8145e451f2f4ec81f22976eb2bfd73a491b9"
+
+            }
+        });
+
+        let jsonFinalResponse = await finalResponse.json()
+
+        if (jsonFinalResponse.status === "succeeded") {
+            generatedImage = jsonFinalResponse.output;
+
+        }else if(jsonFinalResponse.status === "failed"){
+            console.log("jsonFin", jsonFinalResponse);
+            break
+        } else {
+            await new Promise(resolve => setTimeout(resolve, 1000))
+        }
+
+
+
+    }
+
+    res.status(200).json( generatedImage ? generatedImage : "Failed to restore")
 }
